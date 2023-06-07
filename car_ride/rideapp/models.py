a89e5d24679344b2af05eb0ef3f05c69f659dbe6
root <root@Capstone.myguest.virtualbox.org>
2023-06-07 13:48:39 -0400

    Base File with Auth api

---
+++ car_ride/rideapp/models.py
@@ -0,0 +1,112 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+# class User(models.Model):
+# 	user_id = models.IntegerField(primary_key=True)
+# 	first_name = models.CharField(max_length=100)
+# 	last_name = models.CharField(max_length=100)
+# 	age = models.IntegerField()
+# 	dob = models.CharField(max_length=100)
+# 	email = models.CharField(max_length=100)
+# 	phone = models.CharField(max_length=100)
+# 	senior_citizen = models.BooleanField()
+# 	id_proof = models.ImageField()
+# 	is_pass = models.BooleanField()
+# 	pass_from_date = models.DateTimeField()
+# 	pass_to_date = models.DateTimeField()
+# 	create_date = models.DateTimeField(default=timezone.now)
+# 	write_date = models.DateTimeField(auto_now=True)
+
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+class Driver(models.Model):
+	
+	driver_id = models.IntegerField(primary_key=True)
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	age = models.IntegerField()
+	dob = models.CharField(max_length=100)
+	email = models.CharField(max_length=100)
+	phone = models.CharField(max_length=100)
+	license_no = models.CharField(max_length=100)
+	car_year = models.IntegerField()
+	car_model = models.IntegerField()
+	car_name = models.CharField(max_length=100)
+	car_no = models.CharField(max_length=100)
+	create_date = models.DateTimeField(default=timezone.now)
+	write_date = models.DateTimeField(auto_now=True)
+	# create_id = models.ForeignKey(auth_user, on_delete=models.CASCADE)
+	# write_id = models.ForeignKey(auth_user, on_delete=models.CASCADE)
+
+class DriverLocations(models.Model):
+
+	driver_loc_id = models.IntegerField(primary_key=True)
+	driver_id = models.ForeignKey(Driver, on_delete=models.CASCADE)
+	locations = models.CharField(max_length=100)
+	create_date = models.DateTimeField(default=timezone.now)
+	write_date = models.DateTimeField(auto_now=True)
+
+class CarAmenities(models.Model):
+
+	amenities_id = models.IntegerField(primary_key=True)
+	driver_id = models.ForeignKey(Driver, on_delete=models.CASCADE)
+	features = models.CharField(max_length=100)
+	create_date = models.DateTimeField(default=timezone.now)
+	write_date = models.DateTimeField(auto_now=True)
+
+class CarRide(models.Model):
+
+	ride_id = models.IntegerField(primary_key=True)
+	# user_id = models.ForeignKey(AnonymousUser, on_delete=models.CASCADE)
+	driver_id = models.ForeignKey(Driver, on_delete=models.CASCADE)
+	from_loc = models.CharField(max_length=100)
+	to_loc = models.CharField(max_length=100)
+	price = models.DecimalField(max_digits=6, decimal_places=2)
+	pay_type = models.CharField(max_length=100)
+	create_date = models.DateTimeField(default=timezone.now)
+	write_date = models.DateTimeField(auto_now=True)
+
+class CarPool(models.Model):
+
+	pool_id = models.IntegerField(primary_key=True)
+	# user_id = models.ForeignKey(AnonymousUser, on_delete=models.CASCADE)
+	driver_id = models.ForeignKey(Driver, on_delete=models.CASCADE)
+	from_loc = models.CharField(max_length=100)
+	to_loc = models.CharField(max_length=100)
+	price = models.DecimalField(max_digits=6, decimal_places=2)
+	available_seats = models.IntegerField()
+	create_date = models.DateTimeField(default=timezone.now)
+	write_date = models.DateTimeField(auto_now=True)
+
+class Invoice(models.Model):
+
+	invoice_id = models.IntegerField(primary_key=True)
+	ride_id = models.ForeignKey(CarRide, on_delete=models.CASCADE)
+	pool_id = models.ForeignKey(CarPool, on_delete=models.CASCADE)
+	# user_id = models.ForeignKey(AnonymousUser, on_delete=models.CASCADE)
+	driver_id = models.ForeignKey(Driver, on_delete=models.CASCADE)
+	total_payment = models.DecimalField(max_digits=6, decimal_places=2)
+	pay_type = models.CharField(max_length=100)
+	create_date = models.DateTimeField(default=timezone.now)
+	write_date = models.DateTimeField(auto_now=True)
+
+class Payment(models.Model):
+
+	payment_id = models.IntegerField(primary_key=True)
+	invoice_id = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+	# user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	transaction_id = models.CharField(max_length=100)
+	mode_of_pay = models.CharField(max_length=100)
+	price = models.DecimalField(max_digits=6, decimal_places=2)
+	create_date = models.DateTimeField(default=timezone.now)
+	write_date = models.DateTimeField(auto_now=True)
---
+++ car_ride/rideapp/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer,RegisterSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import generics
+from django.contrib.auth import login
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.views import LoginView as KnoxLoginView
+
+
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
+
+class ListUsers(APIView):
+    """
+    View to list all users in the system.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        usernames = [user.username for user in User.objects.all()]
+        return Response(usernames)
+
+
+class RegisterUserAPIView(generics.CreateAPIView):
+	permission_classes = (AllowAny,)
+	serializer_class = RegisterSerializer
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)
